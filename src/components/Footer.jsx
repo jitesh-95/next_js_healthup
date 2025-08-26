883e7391689b7dfbff5ad562791c20de03a257ff
jitesh-95 <1995sirohiss@gmail.com>
2025-08-26 16:18:17 +0530

    dynamic version added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,10 +2,11 @@
 
 import { useThemeMode } from '@/app/ThemeContext';
 import { Box, Typography } from '@mui/material';
-import React from 'react'
+import React from 'react';
+import pgk from '../../package.json';
 
 
-const APP_VERSION = '1.0.0'; // Update from package.json if needed
+const APP_VERSION = pgk.version; // Update from package.json if needed
 
 const Footer = () => {
   const { darkMode } = useThemeMode();
